1e29749428dea12b6662dfb0704cd757d3876e10
chitti239 <sharinis239@gmail.com>
2025-04-01 13:57:36 +0530

    deployme

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h2>Hello..!,Welcome to my page</h2>
+      <h2>This is Sharini S</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
